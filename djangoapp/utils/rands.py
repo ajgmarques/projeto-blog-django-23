364c41e92cf42ae908187801a55e2eaaf558aac8
António Marques <janacorreio@gmail.com>
2023-12-23 02:09:28 -0300

    Models Category Tags e Page

---
+++ djangoapp/utils/rands.py
@@ -0,0 +1,13 @@
+import string
+from random import SystemRandom
+from django.utils.text import slugify
+
+
+def random_letters(k=8):
+    return ''.join(SystemRandom().choices(
+        string.ascii_letters + string.digits,
+        k=k
+    ))
+
+def slugify_new(text, k=8):
+    return slugify(text) + '-' + random_letters(k)
