619122a88e514bbe75162779fa5faf1834fb8d75
António Marques <janacorreio@gmail.com>
2023-12-27 00:21:14 -0300

    Class based views - implementando ListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import (post, page, CreatedByListView, 
-                        category, tag, search, PostListView)
+from blog.views import (post, page, CreatedByListView,
+                        CategoryListView, TagListView,
+                        SearchListView, PostListView)
 
 
 app_name = 'blog'
@@ -13,7 +14,10 @@ urlpatterns = [
         'created_by/<int:author_pk>/',
         CreatedByListView.as_view(), name='created_by'
         ),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
-    path('search/', search, name='search'),
+    path(
+        'category/<slug:slug>/',
+        CategoryListView.as_view(), name='category'
+        ),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404, HttpRequest, HttpResponse
@@ -20,7 +20,6 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -82,71 +81,6 @@ class CreatedByListView(PostListView):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self._temp_context: dict[str, Any] = {}
-    # # VER A ORDEM EM QUE OS MÉTODOS SÃO EXECUTADOS:
-
-    # def setup(self, *args, **kwargs):
-    #     super_setup = super().setup(*args, **kwargs)
-    #     print('Método setup')
-    #     return super_setup
-
-
-    # def dispatch(self, *args, **kwargs):
-    #     super_dispatch = super().dispatch(*args, **kwargs)
-    #     print('Método dispatch')
-    #     return super_dispatch
-
-
-    # def get(self, *args, **kwargs):
-    #     super_get = super().get(*args, **kwargs)
-    #     print('Método get')
-    #     return super_get
-
-
-    # def get_queryset(self, *args, **kwargs):
-    #     super_get_queryset = super().get_queryset(*args, **kwargs)
-    #     print('Método get_queryset')
-    #     return super_get_queryset
-
-
-    # def get_context_data(self, *args, **kwargs):
-    #         super_get_context_data = super().get_context_data(*args, **kwargs)
-    #         print('Método get_context_data')
-    #         return super_get_context_data
-
-
-    # def get_context_object_name(self, *args, **kwargs):
-    #         super_get_context_object_name = super().get_context_object_name(*args, **kwargs)
-    #         print('Método get_context_object_name')
-    #         return super_get_context_object_name
-
-
-    # def get_template_names(self, *args, **kwargs):
-    #         super_get_template_names = super().get_template_names(*args, **kwargs)
-    #         print('Método get_template_names')
-    #         return super_get_template_names
-
-
-    # def render_to_response(self, *args, **kwargs):
-    #         super_render_to_response = super().render_to_response(*args, **kwargs)
-    #         print('Método render_to_response')
-    #         return super_render_to_response
-
-
-    # def http_method_not_allowed(self, *args, **kwargs):
-    #         super_http_method_not_allowed = super().http_method_not_allowed(*args, **kwargs)
-    #         print('Método http_method_not_allowed')
-    #         return super_http_method_not_allowed
-
-    # Ordem dos métodos que é executada:
-    # Método setup
-    # Método get_queryset
-    # Método get_context_object_name
-    # Método get_context_data
-    # Método get_template_names
-    # Método render_to_response
-    # Método get
-    # Método dispatch
-
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
@@ -184,99 +118,76 @@ class CreatedByListView(PostListView):
         return super().get(request, *args, **kwargs)
 
 
-# def created_by(request, author_pk):
-#     user = User.objects.filter(pk=author_pk).first()
+class CategoryListView(PostListView):
+    allow_empty = False
 
-#     if user is None:
-#         raise Http404()
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
 
-#     posts = Post.objects.get_published().filter(created_by__pk=author_pk)
-#     user_full_name = user.username
-#     if user.first_name:
-#         user_full_name = f'{user.first_name} {user.last_name}'
-#     page_title = 'Posts de ' + user_full_name + ' - '
-
-#     paginator = Paginator(posts, PER_PAGE)
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': page_title,
-#         }
-#     )
-
-
-def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug)
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    if len(page_obj) == 0:
-        raise Http404
-
-    page_title = f'Categoria - {page_obj[0].category.name} - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        print(ctx)
+        page_title = (
+            f'Categoria - '
+            f'{self.object_list[0].category.name} - '  # type: ignore
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
-def tag(request, slug):
-    posts = Post.objects.get_published().filter(tags__slug=slug)
+class TagListView(PostListView):
+    allow_empty = False
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            tags__slug=self.kwargs.get('slug')
+        )
 
-    if len(page_obj) == 0:
-        raise Http404
-
-    page_title = f'Tag - {page_obj[0].tags.first().name} - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        print(ctx)
+        page_title = (
+            f'Tag - '
+            f'{self.object_list[0].tags.first().name} - '  # type: ignore
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
-def search(request):
-    search_value = request.GET.get('search', '').strip()
+class SearchListView(PostListView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._search_value = ''
 
-    posts = (
-        Post.objects.get_published().filter(
+    def setup(self, request, *args, **kwargs):
+        self._search_value = request.GET.get('search', '').strip()
+        return super().setup(request, *args, **kwargs)
 
-            # title -> contém search value OU
+    def get_queryset(self) -> QuerySet[Any]:
+        search_value = self._search_value
+        return super().get_queryset().filter(
             Q(title__contains=search_value) |
-            # excerpt -> contém search value OU
             Q(excerpt__contains=search_value) |
-            # content -> conteém search value
             Q(content__contains=search_value)
         )[:PER_PAGE]
-    )
 
-    page_title = f'{search_value[:10]} - Search - '
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        search_value = self._search_value
 
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
+        ctx.update({
+            'page_title':  f'{search_value[:10]} - Search - ',
             'search_value': search_value,
-            'page_title': page_title,
-        }
-    )
+        })
+        return ctx
+
+    def get(self, request, *args, **kwargs):
+        if self._search_value == '':
+            return redirect('blog:index')
+        return super().get(request, *args, **kwargs)
